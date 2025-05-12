0045732d976f0aa5a9066ec5e1bc2bf061ce02eb
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-12 23:26:13 +0200

    model-updates

--- src/main/java/com/infrastructuretool/faultreporting/repository/AttachmentRepository.java
+++ src/main/java/com/infrastructuretool/faultreporting/repository/AttachmentRepository.java
@@ -2,6 +2,8 @@ package com.infrastructuretool.faultreporting.repository;
 
 import com.infrastructuretool.faultreporting.model.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
 }
