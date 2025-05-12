797960dc917b4865f9c14ac83fea68a4b7e5dcbe
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-12 23:29:51 +0200

    model-updates

--- src/main/java/com/infrastructuretool/faultreporting/model/FaultReport.java
+++ src/main/java/com/infrastructuretool/faultreporting/model/FaultReport.java
@@ -36,4 +36,84 @@ public class FaultReport {
     private User reportedBy;
 
     private LocalDateTime reportedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public FaultStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(FaultStatus status) {
+        this.status = status;
+    }
+
+    public User getReportedBy() {
+        return reportedBy;
+    }
+
+    public void setReportedBy(User reportedBy) {
+        this.reportedBy = reportedBy;
+    }
+
+    public LocalDateTime getReportedAt() {
+        return reportedAt;
+    }
+
+    public void setReportedAt(LocalDateTime reportedAt) {
+        this.reportedAt = reportedAt;
+    }
 }
