9fe47bbaa1d35e94b230be932e12d2aceb845981
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-23 23:25:26 +0200

    repo-changes

--- src/main/java/com/infrastructuretool/faultreporting/service/UserServiceImplementation.java
+++ src/main/java/com/infrastructuretool/faultreporting/service/UserServiceImplementation.java
@@ -1,12 +1,21 @@
 package com.infrastructuretool.faultreporting.service;
 
 import com.infrastructuretool.faultreporting.model.User;
+import com.infrastructuretool.faultreporting.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImplementation implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImplementation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public List<User> getAllUsers() {
