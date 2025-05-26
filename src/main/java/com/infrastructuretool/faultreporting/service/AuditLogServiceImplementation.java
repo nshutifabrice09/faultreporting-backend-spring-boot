55aaca44a265002b75251273aad309d03f1d94ca
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-26 09:10:03 +0200

    service-impl-commit

--- src/main/java/com/infrastructuretool/faultreporting/service/AuditLogServiceImplementation.java
+++ src/main/java/com/infrastructuretool/faultreporting/service/AuditLogServiceImplementation.java
@@ -1,12 +1,22 @@
 package com.infrastructuretool.faultreporting.service;
 
 import com.infrastructuretool.faultreporting.model.AuditLog;
+import com.infrastructuretool.faultreporting.repository.AuditLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AuditLogServiceImplementation implements AuditLogService{
+
+    private final AuditLogRepository auditLogRepository;
+
+    @Autowired
+    public AuditLogServiceImplementation(AuditLogRepository auditLogRepository) {
+        this.auditLogRepository = auditLogRepository;
+    }
+
     @Override
     public List<AuditLog> getAllAuditLogs() {
         return null;
