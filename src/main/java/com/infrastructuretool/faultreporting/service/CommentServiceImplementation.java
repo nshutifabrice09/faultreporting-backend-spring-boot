e19765273c3fa2c692c270a236ba53c9120193f4
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-26 09:27:02 +0200

    service-impl-commit

--- src/main/java/com/infrastructuretool/faultreporting/service/CommentServiceImplementation.java
+++ src/main/java/com/infrastructuretool/faultreporting/service/CommentServiceImplementation.java
@@ -1,12 +1,26 @@
 package com.infrastructuretool.faultreporting.service;
 
 import com.infrastructuretool.faultreporting.model.Comment;
+import com.infrastructuretool.faultreporting.repository.CommentRepository;
+import com.infrastructuretool.faultreporting.repository.FaultReportRepository;
+import com.infrastructuretool.faultreporting.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CommentServiceImplementation implements CommentService{
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final FaultReportRepository faultReportRepository;
+
+    public CommentServiceImplementation(CommentRepository commentRepository, UserRepository userRepository, FaultReportRepository faultReportRepository) {
+        this.commentRepository = commentRepository;
+        this.userRepository = userRepository;
+        this.faultReportRepository = faultReportRepository;
+    }
+
     @Override
     public List<Comment> getAllComments() {
         return null;
